5c5396ad96aa84eb9ca0d521a2dd5985eb99a65e
shobhit55 <34087998+shobhit55@users.noreply.github.com>
2021-05-15 16:56:18 +0530

    s

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from models import RRN, Resnet, BasicBlockBN
 from datasets import sudoku_char_data, sudoku_data_8, sudoku_data_train, sudoku_data_8_test
-from train_loops import train_classifier, train_rrn, test_rrn
+from train_loops import train_classifier, train_rrn, test_rrn, clustering
 import numpy as np
 import torch
 import torch.nn as nn
@@ -25,73 +25,75 @@ train_data_dir = args.train_data_dir
 test_data_dir = args.test_data_dir
 sample_file = args.sample_file
 
-#Clustering
-print('Clustering started')
-num_clusters = 8
-dataset = sudoku_char_data(train_data_dir+'/target')
-true_x = torch.tensor(np.load(sample_file), dtype=torch.float32)
-true_x = true_x.view(true_x.shape[0], -1)
+#Clustering, train_data_dir, sample_file
 
-x_all_0 = np.zeros((len(dataset), 784))
-labels_all_0 = np.zeros(len(dataset))
-dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2, drop_last=True)
+sudoku_digits_data = clustering(train_data_dir, sample_file, device=device)
 
-k=0
-for i, x in enumerate(dataloader):
-    x = x.view(x.shape[0], -1)
-    x_new = x
-    cluster = KMeans(n_clusters=num_clusters, random_state=0, verbose=False, max_iter=300, tol=1e-6)
-    cluster.fit(x_new)
-    labels = list(cluster.labels_)
-    true_labels = list(cluster.predict(true_x[1:-1]))
+# print('Clustering started')
+# num_clusters = 8
+# dataset = sudoku_char_data(train_data_dir+'/target')
+# true_x = torch.tensor(np.load(sample_file), dtype=torch.float32)
+# true_x = true_x.view(true_x.shape[0], -1)
 
-    try:
-        labels = list(map(lambda x: true_labels.index(x), labels))
-        x_all_0[k:k+len(x),:] = x
-        labels_all_0[k:k+len(x)] = np.array(labels)+1
-        k = k+len(x)
-    except:
-        print('continued')
-    break
-x_all_0 = x_all_0[:k]
-labels_all_0 = labels_all_0[:k]
+# x_all_0 = np.zeros((len(dataset), 784))
+# labels_all_0 = np.zeros(len(dataset))
+# dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2, drop_last=True)
 
-x_7_9 = x_all_0[(labels_all_0==4) | (labels_all_0==7)]
-cluster = KMeans(n_clusters=2, random_state=0, verbose=False, max_iter=300, tol=1e-6)
-cluster.fit(x_7_9[:,28*4:17*28])
-labels = list(cluster.labels_)
-true_labels = list(cluster.predict(true_x[[4,7],28*4:17*28]))
-labels = list(map(lambda x: true_labels.index(x), labels))
-la = [4,7]
-labels = list(map(lambda x: la[x], labels))
-labels_all_0[(labels_all_0==4) | (labels_all_0==7)] = labels
+# k=0
+# for i, x in enumerate(dataloader):
+#     x = x.view(x.shape[0], -1)
+#     x_new = x
+#     cluster = KMeans(n_clusters=num_clusters, random_state=0, verbose=False, max_iter=300, tol=1e-6)
+#     cluster.fit(x_new)
+#     labels = list(cluster.labels_)
+#     true_labels = list(cluster.predict(true_x[1:-1]))
 
-dataset = sudoku_char_data(train_data_dir+'/query')
-dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2)
+#     try:
+#         labels = list(map(lambda x: true_labels.index(x), labels))
+#         x_all_0[k:k+len(x),:] = x
+#         labels_all_0[k:k+len(x)] = np.array(labels)+1
+#         k = k+len(x)
+#     except:
+#         print('continued')
+# x_all_0 = x_all_0[:k]
+# labels_all_0 = labels_all_0[:k]
 
-x = next(iter(dataloader))
-x = x.view(x.shape[0], -1)
-pca = PCA(n_components=6) #6
-x_new = pca.fit_transform(x)
-cluster = KMeans(n_clusters=num_clusters, random_state=0, tol=1e-6)
-cluster.fit(x_new)
-labels = list(cluster.labels_)
-true_label = cluster.predict(pca.transform(true_x[0:1]))
-dist = []
-def myfunc(e):
-    return e[1]
-for j in range(num_clusters):
-    dist.append([j, distance.cosine(pca.transform(true_x[0:1]), cluster.cluster_centers_[j])])
-dist.sort(key=myfunc)
-labels = np.array(labels)
-x_0 = x[(labels==dist[0][0]) | (labels==dist[1][0]) | (labels==dist[2][0])]
-labels_0 = np.zeros(len(x_0))
-x_all_0 = np.concatenate((x_all_0, x_0), axis=0)
-labels_all_0 = np.concatenate((labels_all_0, labels_0))
-labels_all_0 = np.expand_dims(labels_all_0, 1)
-sudoku_digits_data = np.concatenate((labels_all_0, x_all_0), axis=1)
-np.random.shuffle(sudoku_digits_data)
-print('Clustering done...')
+# x_7_9 = x_all_0[(labels_all_0==4) | (labels_all_0==7)]
+# cluster = KMeans(n_clusters=2, random_state=0, verbose=False, max_iter=300, tol=1e-6)
+# cluster.fit(x_7_9[:,28*4:17*28])
+# labels = list(cluster.labels_)
+# true_labels = list(cluster.predict(true_x[[4,7],28*4:17*28]))
+# labels = list(map(lambda x: true_labels.index(x), labels))
+# la = [4,7]
+# labels = list(map(lambda x: la[x], labels))
+# labels_all_0[(labels_all_0==4) | (labels_all_0==7)] = labels
+
+# dataset = sudoku_char_data(train_data_dir+'/query')
+# dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2)
+
+# x = next(iter(dataloader))
+# x = x.view(x.shape[0], -1)
+# pca = PCA(n_components=6) #6
+# x_new = pca.fit_transform(x)
+# cluster = KMeans(n_clusters=num_clusters, random_state=0, tol=1e-6)
+# cluster.fit(x_new)
+# labels = list(cluster.labels_)
+# true_label = cluster.predict(pca.transform(true_x[0:1]))
+# dist = []
+# def myfunc(e):
+#     return e[1]
+# for j in range(num_clusters):
+#     dist.append([j, distance.cosine(pca.transform(true_x[0:1]), cluster.cluster_centers_[j])])
+# dist.sort(key=myfunc)
+# labels = np.array(labels)
+# x_0 = x[(labels==dist[0][0]) | (labels==dist[1][0]) | (labels==dist[2][0])]
+# labels_0 = np.zeros(len(x_0))
+# x_all_0 = np.concatenate((x_all_0, x_0), axis=0)
+# labels_all_0 = np.concatenate((labels_all_0, labels_0))
+# labels_all_0 = np.expand_dims(labels_all_0, 1)
+# sudoku_digits_data = np.concatenate((labels_all_0, x_all_0), axis=1)
+# np.random.shuffle(sudoku_digits_data)
+# print('Clustering done...')
 
 #Classifier------------------------------------------------------------------------------------------------------------
 optim_key = 'SGD'
--- train_loops.py
+++ train_loops.py
@@ -1,14 +1,87 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 import numpy as np
 from tqdm import tqdm
 from functools import partial
+from datasets import sudoku_char_data
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from scipy.spatial import distance
 tqdm = partial(tqdm, position=0, leave=True)
+
+def clustering(train_data_dir, sample_file, device=torch.device('cuda')):
+    print('Clustering started')
+    num_clusters = 8
+    dataset = sudoku_char_data(train_data_dir+'/target')
+    true_x = torch.tensor(np.load(sample_file), dtype=torch.float32)
+    true_x = true_x.view(true_x.shape[0], -1)
+
+    x_all_0 = np.zeros((len(dataset), 784))
+    labels_all_0 = np.zeros(len(dataset))
+    dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2, drop_last=True)
+
+    k=0
+    for i, x in enumerate(dataloader):
+        x = x.view(x.shape[0], -1)
+        x_new = x
+        cluster = KMeans(n_clusters=num_clusters, random_state=0, verbose=False, max_iter=300, tol=1e-6)
+        cluster.fit(x_new)
+        labels = list(cluster.labels_)
+        true_labels = list(cluster.predict(true_x[1:-1]))
+
+        try:
+            labels = list(map(lambda x: true_labels.index(x), labels))
+            x_all_0[k:k+len(x),:] = x
+            labels_all_0[k:k+len(x)] = np.array(labels)+1
+            k = k+len(x)
+        except:
+            print('continued')
+    x_all_0 = x_all_0[:k]
+    labels_all_0 = labels_all_0[:k]
+
+    x_7_9 = x_all_0[(labels_all_0==4) | (labels_all_0==7)]
+    cluster = KMeans(n_clusters=2, random_state=0, verbose=False, max_iter=300, tol=1e-6)
+    cluster.fit(x_7_9[:,28*4:17*28])
+    labels = list(cluster.labels_)
+    true_labels = list(cluster.predict(true_x[[4,7],28*4:17*28]))
+    labels = list(map(lambda x: true_labels.index(x), labels))
+    la = [4,7]
+    labels = list(map(lambda x: la[x], labels))
+    labels_all_0[(labels_all_0==4) | (labels_all_0==7)] = labels
+
+    dataset = sudoku_char_data(train_data_dir+'/query')
+    dataloader = DataLoader(dataset, batch_size=int(len(dataset)/8), num_workers=0 if device==torch.device('cpu') else 2)
+
+    x = next(iter(dataloader))
+    x = x.view(x.shape[0], -1)
+    pca = PCA(n_components=6) #6
+    x_new = pca.fit_transform(x)
+    cluster = KMeans(n_clusters=num_clusters, random_state=0, tol=1e-6)
+    cluster.fit(x_new)
+    labels = list(cluster.labels_)
+    true_label = cluster.predict(pca.transform(true_x[0:1]))
+    dist = []
+    def myfunc(e):
+        return e[1]
+    for j in range(num_clusters):
+        dist.append([j, distance.cosine(pca.transform(true_x[0:1]), cluster.cluster_centers_[j])])
+    dist.sort(key=myfunc)
+    labels = np.array(labels)
+    x_0 = x[(labels==dist[0][0]) | (labels==dist[1][0]) | (labels==dist[2][0])]
+    labels_0 = np.zeros(len(x_0))
+    x_all_0 = np.concatenate((x_all_0, x_0), axis=0)
+    labels_all_0 = np.concatenate((labels_all_0, labels_0))
+    labels_all_0 = np.expand_dims(labels_all_0, 1)
+    sudoku_digits_data = np.concatenate((labels_all_0, x_all_0), axis=1)
+    np.random.shuffle(sudoku_digits_data)
+    print('Clustering done...')
+    return sudoku_digits_data
 
 def train_rrn(model, train_loader, class_model, device=torch.device('cuda')):
     print("RRN Training Started...")
     wd = 1e-4
-    epochs = 1
+    epochs = 53 #-------------------------------------------
     lr = 2e-4
     optim_key = 'Adam'
     optimizer_dict = {"AdaDel": torch.optim.Adadelta(model.parameters(), weight_decay=wd), "Adam": torch.optim.Adam(model.parameters(), lr = lr, weight_decay = wd), "SGD": torch.optim.SGD(model.parameters(), lr = lr, weight_decay=wd)}
