0624bddec3afa51ae547d3726a4ca16f3cda16f5
shobhit55 <34087998+shobhit55@users.noreply.github.com>
2021-05-15 17:01:48 +0530

    s

--- run_solver.sh
+++ run_solver.sh
@@ -4,5 +4,6 @@ conda install pytorch --y
 conda install scikit-learn --y
 conda install torchvision --y
 conda install tqdm --y
+conda install numpy --y
 
 python3 main.py --train_data_dir $1 --test_data_dir $2 --sample_file $3 --output_file $4
